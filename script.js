754cbd31100fc65571cf6661aec64ef511cd9483
RanjithFSD <kranjith868@gmail.com>
2024-07-23 01:22:22 -0500

    sample code added

---
+++ script.js
@@ -0,0 +1,53 @@
+let myResume={
+    "basics": {
+      "name": "Ranjith Kumar Kalimuth",
+      "email": "kranjith868@gamil.com",
+      "phone": 9043634270,
+      "degree": "B.E",
+      "location": {
+        "address": "Keelappavoor",
+        "postalCode": 627806,
+        "city": "Tenkasi",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      },
+    "work": [
+      {
+        "company": "Kitkool",
+        "position": "Technical Recruiter",
+        "startDate": "Feb 2024",
+        "endDate": "Till Now",
+        "summary": "Highly skilled US Technical Recruiter with over 2 years of experience in sourcing, screening, and placing IT professionals in various roles such as software developers, network engineers, and system administrators. Proven track record of reducing time-to-hire by 30% and increasing qualified applicant flow by 25% through effective recruitment strategies and data management systems. Adept at utilizing CATS ATS platform for end-to-end recruitment processes and maintaining a talent pipeline of over 500 passive candidates. Strong collaborator with hiring managers to ensure alignment on candidate qualifications and cultural fit. Achieved a 95% acceptance rate on job offers and seamless onboarding of new hires. Experienced in generating detailed recruitment reports and analytics for continuous improvement.",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Einstein College Of Engineering",
+        "department": "Electronics and communication Engineering",
+        "studyType": "Fulltime",
+        "batch": 2015,
+        "gpa": 7.2,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Javascript",
+        "level": "begginer",
+        "project": [
+          "WildLens Tours Using MERN Stack"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Coding,",
+      }
+    ]
+  }
+  console.log(myResume);
